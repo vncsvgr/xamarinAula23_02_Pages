c3a4ea1e129faa04dd7011b9b8ee3706df4cfd7e
vncsvgr <vinicius.gabriel83@yahoo.com>
2022-02-23 22:03:56 -0300

    Adicionar arquivos de projeto.

---
+++ appAula23_02_Pages/appAula23_02_Pages/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appAula23_02_Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFinalizar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
